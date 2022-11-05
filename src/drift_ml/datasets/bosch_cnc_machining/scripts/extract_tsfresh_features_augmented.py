52c4fc04674d3734e97f143a633d787534c23e26
Nicolas Jourdan <nicolas.jourdan@tu-darmstadt.de>
2022-11-05 21:46:33 +0100

    Add script to extract augmented tsfresh features

---
+++ src/drift_ml/datasets/bosch_cnc_machining/scripts/extract_tsfresh_features_augmented.py
@@ -0,0 +1,71 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation
+from tsfresh import feature_extraction
+
+from drift_ml.datasets.bosch_cnc_machining.utils.dataloader import NPYBoschCNCDataLoader
+from drift_ml.datasets.bosch_cnc_machining.utils.utils import (
+    augment_xyz_samples,
+    extract_tsfresh_features,
+)
+
+if __name__ == "__main__":
+    loader = NPYBoschCNCDataLoader(
+        metadata_path="/home/tbiegel/nico_files/bosch_cnc_machining/features_and_data/metadata_ws4096.pkl"
+    )
+    loader.load_data(
+        "/home/tbiegel/nico_files/bosch_cnc_machining/features_and_data/sample_data_x_raw_ws4096.npy",
+        "/home/tbiegel/nico_files/bosch_cnc_machining/features_and_data/sample_data_y_raw_ws4096.npy",
+    )
+
+    features = pd.read_pickle(
+        "/home/tbiegel/nico_files/bosch_cnc_machining/features_and_data/all_top_30_features.pkl"
+    )
+    feature_settings = feature_extraction.settings.from_columns(features)
+
+    target_path = "/home/tbiegel/nico_files/drift_ml/src/drift_ml/datasets/bosch_cnc_machining/raw_data/augmented"
+
+    min_degrees = 1.0
+    max_degrees = 20.0
+    step_degrees = 2.0
+
+    for shift_degrees in np.arange(min_degrees, max_degrees, step_degrees):
+        print(f"Processing features for a shift of {shift_degrees}...")
+
+        # First, yaw
+        filepath = os.path.join(
+            target_path, f"tsfresh_top30_yaw_shift_{shift_degrees}_deg.pkl"
+        )
+        augmented_samples = augment_xyz_samples(
+            loader.sample_data_X, yaw_deg=shift_degrees
+        )
+        extracted_features = extract_tsfresh_features(
+            augmented_samples, feature_settings
+        )
+        extracted_features.to_pickle(filepath)
+
+        # Second, pitch
+        filepath = os.path.join(
+            target_path, f"tsfresh_top30_pitch_shift_{shift_degrees}_deg.pkl"
+        )
+        augmented_samples = augment_xyz_samples(
+            loader.sample_data_X, pitch_deg=shift_degrees
+        )
+        extracted_features = extract_tsfresh_features(
+            augmented_samples, feature_settings
+        )
+        extracted_features.to_pickle(filepath)
+
+        # Last, roll
+        filepath = os.path.join(
+            target_path, f"tsfresh_top30_roll_shift_{shift_degrees}_deg.pkl"
+        )
+        augmented_samples = augment_xyz_samples(
+            loader.sample_data_X, roll_deg=shift_degrees
+        )
+        extracted_features = extract_tsfresh_features(
+            augmented_samples, feature_settings
+        )
+        extracted_features.to_pickle(filepath)
